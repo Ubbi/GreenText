9b5f656b2d3aa0138a2a8c2d242ddf6d9a54312a
Ubbi Dubbi <dubbiubbi@gmail.com>
2012-05-31 14:38:39 -0400

    Made event to change text green

--- src/main/java/com/gmail/dubbiubbi/greentext/GreenText.java
+++ src/main/java/com/gmail/dubbiubbi/greentext/GreenText.java
@@ -1,15 +1,28 @@
 package com.gmail.dubbiubbi.greentext;
 
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class GreenText extends JavaPlugin {
+public class GreenText extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+
 		this.getLogger().info("GreenText loaded!");
 	}
 
 	@Override
 	public void onDisable() {
 		this.getLogger().info("GreenText unloaded!");
+	}
+
+	@EventHandler
+	public void onPlayerChat(PlayerChatEvent event) {
+		String message = event.getMessage();
+		message = ChatColor.GREEN + message;
+		event.setMessage(message);
 	}
 }
